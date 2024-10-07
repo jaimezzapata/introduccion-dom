cff6cc90f336091c60c03867133c6adedfe83658
jaimezzapata <zapataval2304@gmail.com>
2024-10-07 12:27:39 -0500

    Implementación de módulos, funciones y objetos en el DOM

---
+++ model/ModelUsuarios.js
@@ -0,0 +1,11 @@
+export let usuarios = [
+    {
+        usuario: "admin",
+        contrasena: "1234567"
+    },
+    {
+        usuario: 'super-admin',
+        contrasena: '2352345'
+    }
+]
+/* Objetos */
--- view/public/js/script.js
+++ view/public/js/script.js
@@ -9,9 +9,37 @@ querySelector
     querySelectorAll -> Permite seleccionar por id, por clase y por etiqueta
 */
 
-console.log(document.getElementById("usuario")) /* Solo muestra un elemento */
-console.log(document.getElementsByClassName("form")) /* Siempre muestra un arreglo */
-console.log(document.getElementsByTagName("form")) /* Siempre muestra un arreglo */
+// console.log(document.getElementById("usuario")) /* Solo muestra un elemento */
+// console.log(document.getElementsByClassName("form")) /* Siempre muestra un arreglo */
+// console.log(document.getElementsByTagName("form")) /* Siempre muestra un arreglo */
 
-console.log(document.querySelector("form"))
-console.log(document.querySelectorAll("form"))
+// console.log(document.querySelector("form"))
+// console.log(document.querySelectorAll("form"))
+
+import { usuarios } from "../../../model/ModelUsuarios.js"
+
+console.log(usuarios)
+
+function iniciarSesion() {
+    let usuario = document.querySelector('#usuario').value
+    let contrasena = document.querySelector('#contrasena').value
+    if (usuario == 'admin' && contrasena == '12345') {
+        console.log('Bienvenido')
+    } else {
+        console.log('Error de credenciales')
+    }
+}
+
+// let iniciarSesion = function () { /* Función de expresión */
+//     let usuario = document.querySelector('#usuario')
+//     let contrasena = document.querySelector('#contrasena')
+//     console.log(usuario)
+//     console.log(contrasena)
+//     console.log('Iniciar Sesión')
+// }
+// iniciarSesion()
+
+
+
+
+// console.log(usuario)
